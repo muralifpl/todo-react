3ccf3c94da0020c67f91945ea269f928884a1fa2
murali_007 <muraliragu99@gmail.com>
2023-07-21 17:51:36 +0530

    t1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './App.css'
+const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
 
-function App() {
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== "") {
+      if (editIndex !== null) {
+        // Edit existing task
+        const newTodos = [...todos];
+        newTodos[editIndex] = inputValue;
+
+        console.log(newTodos)
+        setTodos(newTodos);
+        setInputValue("");
+        setEditIndex(null);
+
+      } else {
+        // Add new task
+        setTodos([...todos, inputValue]);
+        setInputValue("");
+      }
+    }
+    
+  };
+
+  const handleDeleteTodo = (index) => {
+    const newTodos = todos.filter((sample, i) => i !== index);
+    setTodos(newTodos);
+    
+  };
+
+  const handleEditTodo = (index) => {
+   
+    setEditIndex(index);
+    setInputValue(todos[index]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter your todo..."
+        />
+        <button onClick={handleAddTodo}>{editIndex !== null ? "Save" : "Add"}</button>
+      </div>
+      <ol>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleEditTodo(index)}>Edit</button>
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ol>
     </div>
   );
-}
+};
 
-export default App;
+export default Todo;
